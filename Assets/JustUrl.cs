99332ab12776ff687cfaea967a3b594405805456
SheiinX <sebastianfrancocatano@gmail.com>
2024-02-06 10:33:33 -0500

    ar

---
+++ Assets/JustUrl.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JustUrl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.OpenURL("https://github.com/SheiinX/SSWolfRepository");
+    }
+}
